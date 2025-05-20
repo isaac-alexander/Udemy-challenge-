e9f6da82cb2ab70140b6137a1773c3fc26933a78
Isaac Alexander <Isaacalexander81@gmail.com>
2025-05-20 01:55:52 +0100

    challenge9ObjectDestructuring.js

---
+++ challenge9ObjectDestructuring.js
@@ -0,0 +1,28 @@
+/* 
+
+Declare mult() function that will multiply values
+of the x, y, z fields of the passed object.
+*/
+
+
+
+var obj = {
+    x: 5,
+    y: 20,
+    z: 3
+};
+
+// Write mult() function here
+
+// function mult(object) {
+//     var {x, y, z} = object; 
+//     return x * y * z;
+// } 
+
+//OR
+
+function mult({x, y, z}) {
+    return x * y * z;
+}
+
+console.log(mult(obj));
