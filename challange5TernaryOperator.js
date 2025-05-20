e7bc85f6afbd7d4b12cc5460f91ce407e0123b50
Isaac Alexander <Isaacalexander81@gmail.com>
2025-05-20 01:53:54 +0100

    challange5TernaryOperator.js

---
+++ challange5TernaryOperator.js
@@ -0,0 +1,22 @@
+/* 
+
+Change contents of the isNumber function
+using ternary operator.
+*/
+
+
+
+function isNumber(a) {
+    return typeof a === "number" ? "That's a number" : "That's not a number";
+
+// if (typeof a === "number") {
+//     return "That's a number";
+// } else {
+//     return "That's not a number"
+// }
+
+}
+
+console.log(isNumber(10));
+console.log(isNumber("Hey there"));
+console.log(isNumber(true));
