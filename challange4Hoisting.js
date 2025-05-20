b3172416ca29143229a1ae0adb089f6d7e3fdb0d
Isaac Alexander <Isaacalexander81@gmail.com>
2025-05-20 01:53:29 +0100

    challange4Hoisting.js

---
+++ challange4Hoisting.js
@@ -0,0 +1,27 @@
+/*
+
+Change code to fix first error after line 15
+Error after the line 20 should still be generated.
+*/
+
+
+
+var a = 5,
+    b = 10;
+
+if (b > a) {
+    c = a + b + c;
+    let c = 2;
+    console.log(c);
+    // BEFORE: Uncaught ReferenceError: c is not defined
+    // AFTER: 17
+}
+
+console.log(c);
+// Uncaught ReferenceError: c is not defined
+
+//SOLUTION
+
+//Define c with the let keyword before trying to reassign it. declaration before re-assiging
+// let c = 2;
+// c = a + b + c;
