96c78044295e420ae8bddf2d4e97f895ff72a094
Isaac Alexander <Isaacalexander81@gmail.com>
2025-05-20 01:56:54 +0100

    challenge10DestructuringAndRestOperator.js

---
+++ challenge10DestructuringAndRestOperator.js
@@ -0,0 +1,15 @@
+/* CHALLENGE 10 - Destructuring and Rest Operator 
+
+Assign values to the a, b, c variables
+using destructuring and rest operator.
+*/
+
+
+
+var a, b, c;
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+//Write code here
+
+console.log(a);
