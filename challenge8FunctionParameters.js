1c4584d024942ed1b3976d50a354500d6857fd04
Isaac Alexander <Isaacalexander81@gmail.com>
2025-05-20 01:55:28 +0100

    challenge8FunctionParameters.js

---
+++ challenge8FunctionParameters.js
@@ -0,0 +1,37 @@
+/*
+
+Throw Error when function square() is called
+without arguments.
+
+Create new function and use it as default parameter.
+*/
+
+
+
+function squareA(a) {
+    console.log(a * a);
+
+}
+
+squareA(10); //100
+
+squareA();
+//BEFORE: NaN
+//AFTER: Uncaught Error: Function square requires an argument!
+
+// SOLUTION
+
+function missingArg() {
+    throw new Error("Function square requires an argument!");
+}
+
+
+function square(a = missingArg()) {
+    console.log(a * a);
+
+}
+
+square(10);
+
+
+square();
