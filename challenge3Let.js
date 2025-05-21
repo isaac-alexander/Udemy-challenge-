29e09482a7a1767797e0590cca17bdaaef5de9e5
Isaac Alexander <Isaacalexander81@gmail.com>
2025-05-21 01:50:34 +0100

    challenge3Let.js

---
+++ challenge3Let.js
@@ -0,0 +1,19 @@
+/* CHALLENGE 3 - Let
+
+Change code to match output.
+*/
+
+
+
+var i = 10;
+
+for (var i = 0; i < 5; i++) {
+
+}
+
+console.log(i);
+// BEFORE: 5
+// AFTER: 10
+
+////SOLUTION
+// In the for loop use the let keyword word and log i to the console inside the for loop
